fe6333f8442aa2a3d3919a81fba8094f182c03aa
ValerioDev6 <108197435+ValerioDev6@users.noreply.github.com>
2025-10-16 18:57:35 -0500

    feat: changes datasource driver

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -19,7 +19,6 @@ const config: DataSourceOptions = {
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USER,
-  driver: require('mysql2'),
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   entities: [join(__dirname, '../**/*.entity{.ts,.js}')],
